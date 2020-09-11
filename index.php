e5922726558ff2f9b9e87fb59d1f4a368ef9f9ca
adamjunx <adamhendra79@gmail.com>
2020-09-12 03:01:47 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <style>
+    section{
+        min-height: 420px;
+    }
+    </style>
+    
+    <title>Aisyah's Portfolio</title>
+  </head>
+  <body class="mt-5">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+        <a class="navbar-brand" href="#">Aisyah Putri Ramadhani</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            
+        <div class="smooth-scroll list-unstyled">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#about">About</a>
+            <a class="nav-item nav-link" href="#portfolio">Portfolio</a>
+            <a class="nav-item nav-link" href="#contact" tabindex="-1" aria-disabled="true">Contact</a>
+            </div>
+        </div>
+        </div>
+        </div>
+        </nav>
+
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+        <img src="img/profile.jpg" width="30%" class="rounded-circle img-thumbnail">
+        <br><br>
+        <h1 class="display-4">Hello! Welcome!</h1>
+        <p class="lead">I'm Aisyah Putri Ramadhani, get to know me and this is my portofolio</p>
+        </div>
+        </div>
+
+        <section id="about" class="about bg-primary pb-4">
+        <div class="container">
+        <div class="row mb-5 pt-5">
+        <div class="col text-center">
+        <h2>About Me</h2>
+        </div> 
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="col-md-5 text-justify">
+        <p><h3><small>Born on Thursday, 16th December 1999, Bekasi, Jawa Barat.<br>
+        Im college student, but I do some jobs like content writing, build cooperation for media, sales marketing, social media administration, and editing.</small></h3></p>
+        </div>
+        <div class="col-md-5 text-justify">
+        <p><h3><small>Im currently doing business and marketing advertisement, and also doing editing. Im also doing for looking to companies to cooperate with and put advertisements in our media.</small><h3></p>
+        </div>
+        </div>
+        </div>
+        </section>
+
+        <section id="portfolio" class="portfolio bg-dark pb-4">
+        <div class="container">
+        <div class="row mb-5 pt-5">
+        <div class="col text-center">
+        <font color="white">
+        <h2>Portfolio</h2>
+        </font>
+        </div>
+        </div>
+
+        <div class="row mb-4">
+        
+        <div class="col-md">
+        <div class="card">
+        <img src="img/bulaksumur.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">Social Media Administration</h4>
+        <p class="card-text">I organise social media of SKM UGM Bulaksumur and I write the caption too. here is the Instagram.</p>
+        <a href="https://www.instagram.com/skmugmbul/" class="btn btn-primary">Detail Info</a>
+        </div>
+        </div>
+        </div>
+        <div class="col-md">
+        <div class="card">
+        <img src="img/editing.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">Editing</h4>
+        <p class="card-text">I also edited some articles, like news or papers.</p>
+        <a href="https://www.bulaksumurugm.com/2020/09/09/pelaksanaan-kkn-daring-menurut-mahasiswa/" class="btn btn-primary">Detail Info</a>
+        </div>
+        </div>
+        </div>
+        <div class="col-md">
+        <div class="card">
+        <img src="img/konten.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">Content Creator</h4>
+        <p class="card-text">I make a podcast channel with my friends. I also write the caption for this.</p>
+        <a href="https://open.spotify.com/episode/0MWDilr9RcIN9bFFJ21Ox1" class="btn btn-primary">Detail Info</a>
+        </div>
+        </div>
+        </div>
+        <div class="col-md">
+        <div class="card">
+        <img src="img/magazine.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">Business Marketing Advertisement</h4>
+        <p class="card-text">Here some of Ad that I had for Bulaksumur Magazine.</p>
+        <a href="https://www.bulaksumurugm.com/2019/08/03/bulaksumur-pos-edisi-khusus-mahasiswa-baru-2019/" class="btn btn-primary">Detail Info</a>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </section>
+        
+        <section id="contact" class="contact bg-info mb-10">
+        <div class="container">
+        <div class="row pt-5 mb-5">
+        <div class="col text-center">
+        <h2>Contact</h2>
+        </div>
+        </div>
+        <div class="row justify-content-center">
+        <div class="col-lg-4">
+        <div class="card text-dark bg-warning mb-3">
+        <div class="card-body text-center">
+            <h5 class="card-title">Contact Me!</h5>
+            <p class="card-text">Phone  : 082210088630 <br>
+            Email   : aisaahhp.r@gmail.com </p>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="col-lg-4">
+        <div class="card text-dark bg-light mb-3">
+        <div class="card-body text-center">
+            <h5 class="card-title">Location</h5>
+            <p class="card-text">Perum. Trias Estate E3/10, Wanasari, Cibitung, Bekasi, Jawa Barat,ID.</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.5296043366055!2d107.08271652914107!3d-6.248122966578579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f7674d0c44b%3A0x15a671de26910a5!2sJl.%20Euphorbia%202%2C%20Wanasari%2C%20Kec.%20Cibitung%2C%20Bekasi%2C%20Jawa%20Barat%2017520!5e0!3m2!1sid!2sid!4v1599852204754!5m2!1sid!2sid" width="250" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe><br>
+            <a href="https://goo.gl/maps/p7iMMDcyXWaMxTdH6" class="btn btn-primary">Detail Info</a>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        </div>
+        </div>
+        </section>
+
+        
+     
+
+
+        <footer class="bg-dark text-white">
+        <div class="container pt-3">
+        <div class="row">
+        <div class="col text-center">
+        <p>Instagram : @aisaahp.r || Copyright; 2020</p>
+        </div>
+        </div>
+        </div>
+        </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
